2692c2b0f981174b12d0d3d5c7ecfd9dcde96704
Manuel de Brito Fontes <aledbf@gmail.com>
2016-07-14 18:26:19 -0400

    Use k8s informer and cache. Cleanup

--- core/setup/kubernetes.go
+++ core/setup/kubernetes.go
@@ -1,37 +1,24 @@
 package setup
 
 import (
-	//"crypto/tls"
-	//"crypto/x509"
 	"fmt"
-	//"io/ioutil"
-	//"net"
-	//"net/http"
+	"log"
 	"strings"
-	//"time"
 
 	"github.com/miekg/coredns/middleware"
 	"github.com/miekg/coredns/middleware/kubernetes"
-	k8sc "github.com/miekg/coredns/middleware/kubernetes/k8sclient"
+	k8sc "github.com/miekg/coredns/middleware/kubernetes"
 	"github.com/miekg/coredns/middleware/kubernetes/nametemplate"
-	"github.com/miekg/coredns/middleware/proxy"
-	//"github.com/miekg/coredns/middleware/singleflight"
-
-	"golang.org/x/net/context"
 )
 
 const (
-	defaultK8sEndpoint  = "http://localhost:8080"
-	defaultNameTemplate = "{service}.{namespace}.{zone}"
+	defaultNameTemplate = "${service}.${namespace}.${zone}"
 )
 
 // Kubernetes sets up the kubernetes middleware.
 func Kubernetes(c *Controller) (middleware.Middleware, error) {
 	fmt.Println("controller %v", c)
-	// TODO: Determine if subzone support required
-
 	kubernetes, err := kubernetesParse(c)
-
 	if err != nil {
 		return nil, err
 	}
@@ -43,78 +30,50 @@ func Kubernetes(c *Controller) (middleware.Middleware, error) {
 }
 
 func kubernetesParse(c *Controller) (kubernetes.Kubernetes, error) {
-
-	/*
-	 * TODO: Remove unused state and simplify.
-	 * Inflight and Ctx might not be needed. Leaving in place until
-	 * we take a pass at API caching and optimizing connector to the
-	 * k8s API. Single flight (or limited upper-bound) for inflight
-	 * API calls may be desirable.
-	 */
-
-	k8s := kubernetes.Kubernetes{
-		Proxy: proxy.New([]string{}),
-		Ctx:   context.Background(),
-		//      Inflight:   &singleflight.Group{},
-	}
-	var (
-		endpoints  = []string{defaultK8sEndpoint}
-		template   = defaultNameTemplate
-		namespaces = []string{}
-	)
-
-	k8s.APIConn = k8sc.NewK8sConnector(endpoints[0])
+	k8s := k8sc.NewK8sConnector()
 	k8s.NameTemplate = new(nametemplate.NameTemplate)
-	k8s.NameTemplate.SetTemplate(template)
+	k8s.NameTemplate.SetTemplate(defaultNameTemplate)
 
 	for c.Next() {
 		if c.Val() == "kubernetes" {
 			zones := c.RemainingArgs()
-
+			log.Printf("Zones: %v", zones)
 			if len(zones) == 0 {
 				k8s.Zones = c.ServerBlockHosts
 			} else {
 				// Normalize requested zones
 				k8s.Zones = kubernetes.NormalizeZoneList(zones)
 			}
+			k8s.Zones = []string{"cluster.local"}
 
 			middleware.Zones(k8s.Zones).FullyQualify()
 			if c.NextBlock() {
-				// TODO(miek): 2 switches?
-				switch c.Val() {
+				/*switch c.Val() {
 				case "endpoint":
 					args := c.RemainingArgs()
 					if len(args) == 0 {
 						return kubernetes.Kubernetes{}, c.ArgErr()
 					}
-					endpoints = args
-					k8s.APIConn = k8sc.NewK8sConnector(endpoints[0])
-				}
+				}*/
 				for c.Next() {
 					switch c.Val() {
 					case "template":
 						args := c.RemainingArgs()
-						if len(args) == 0 {
-							return kubernetes.Kubernetes{}, c.ArgErr()
-						}
-						template = strings.Join(args, "")
-						err := k8s.NameTemplate.SetTemplate(template)
-						if err != nil {
-							return kubernetes.Kubernetes{}, err
+						if len(args) != 0 {
+							template := strings.Join(args, "")
+							k8s.NameTemplate.SetTemplate(template)
 						}
 					case "namespaces":
 						args := c.RemainingArgs()
-						if len(args) == 0 {
-							return kubernetes.Kubernetes{}, c.ArgErr()
+						if len(args) != 0 {
+							k8s.Namespaces = &args
 						}
-						namespaces = args
-						k8s.Namespaces = &namespaces
 					}
 				}
 			}
+
 			return k8s, nil
 		}
 	}
-	fmt.Println("here before return")
 	return kubernetes.Kubernetes{}, nil
 }
--- middleware/kubernetes/handler.go
+++ middleware/kubernetes/handler.go
@@ -2,6 +2,7 @@ package kubernetes
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/miekg/coredns/middleware"
 
@@ -10,11 +11,29 @@ import (
 )
 
 func (k Kubernetes) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	fmt.Printf("[debug] here entering ServeDNS: ctx:%v dnsmsg:%v\n", ctx, r)
-
 	state := middleware.State{W: w, Req: r}
 	if state.QClass() != dns.ClassINET {
 		return dns.RcodeServerFailure, fmt.Errorf("can only deal with ClassINET")
+	}
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Authoritative, m.RecursionAvailable, m.Compress = true, true, true
+
+	// TODO: find an alternative to this block
+	if strings.HasSuffix(state.Name(), arpaSuffix) {
+		ip, _ := extractIP(state.Name())
+		records := k.getServiceRecordForIP(ip, state.Name())
+		if len(records) > 0 {
+			srvPTR := &records[0]
+			m.Answer = append(m.Answer, srvPTR.NewPTR(state.QName(), ip))
+
+			m = dedup(m)
+			state.SizeAndDo(m)
+			m, _ = state.Scrub(m)
+			w.WriteMsg(m)
+			return dns.RcodeSuccess, nil
+		}
 	}
 
 	// Check that query matches one of the zones served by this middleware,
@@ -26,10 +45,6 @@ func (k Kubernetes) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.M
 		}
 		return k.Next.ServeDNS(ctx, w, r)
 	}
-
-	m := new(dns.Msg)
-	m.SetReply(r)
-	m.Authoritative, m.RecursionAvailable, m.Compress = true, true, true
 
 	var (
 		records, extra []dns.RR
